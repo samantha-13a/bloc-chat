45df2e4a8cbf1b4fe03c9c2c09aebc3c21707832
Samantha Acampora <samantha.acampora@gmail.com>
2018-11-28 14:58:41 -0500

    Checkpoint 4 List Messages: Complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import './App.css';
 import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
 
 var config = {
   apiKey: "AIzaSyBVA4BoE1bZgKXB-4vnLvVLblxq8M3Unq0",
@@ -14,10 +15,34 @@ var config = {
 firebase.initializeApp(config);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state= { 
+      activeRoomName: '',
+      activeRoomId: ''
+    };
+    this.setActiveRoom = this.setActiveRoom.bind(this);
+  }
+
+  setActiveRoom(room) {
+    this.setState({ 
+      activeRoomName: room.name,
+      activeRoomId: room.key
+     });
+  }
+
   render() {
     return (
       <div className="App">
-          <RoomList firebase={firebase} />
+          <RoomList 
+            firebase={firebase}
+            setActiveRoom={this.setActiveRoom}
+            activeRoomId={this.state.activeRoomId}
+          />
+          <MessageList firebase={firebase} 
+            activeRoomId={this.state.activeRoomId}
+            activeRoomName={this.state.activeRoomName}
+          />
       </div>
     ); 
   }
